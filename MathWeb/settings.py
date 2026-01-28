263e8c96fc11bdfec7ab2ad545f06765419c9743
Tenerec123 <alvarofer@gmail.com>
2026-01-28 16:24:06 +0100

    Pruebo a ver si cambiar el puerto de mail hace que vaya en deploy. Este puerto en local no va al menos

--- MathWeb/settings.py
+++ MathWeb/settings.py
@@ -176,7 +176,7 @@ else:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True  # importante para seguridad
 
 EMAIL_HOST_USER = 'solvesheep@gmail.com'        # tu cuenta de Gmail
