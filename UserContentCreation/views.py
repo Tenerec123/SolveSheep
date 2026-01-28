aba5c65b51ca88cce47c26f5822587b49a759e62
Tenerec123 <alvarofer@gmail.com>
2026-01-28 17:13:58 +0100

    Threading + not silent fail2

--- UserContentCreation/views.py
+++ UserContentCreation/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 import threading
 
-def SendSolutionToVerificate(request, prob_id):
-    if request.method == "GET":
-        return render(request, "send_solution.html", {"prob_id":prob_id})
-    thread = threading.Thread(target=lambda:send_mail(
+def enviar_email_async(prob_id, msg):
+    try:
+        send_mail(
         subject=f'La solución enviada a {prob_id}',
-        message=request.POST.get("solution", "ERROR"),
+        message=msg,
         from_email='solvesheep@gmail.com',
         recipient_list=['solvesheep@gmail.com'],
         fail_silently=False
-    ))
+    )
+        print(f"DEBUG: Correo enviado con éxito para el problema {prob_id}")
+    except Exception as e:
+        # Esto saldrá en los logs de Render sin tumbar la web
+        print(f"ERROR enviando correo: {e}")
+
+
+def SendSolutionToVerificate(request, prob_id):
+    if request.method == "GET":
+        return render(request, "send_solution.html", {"prob_id":prob_id})
+    thread = threading.Thread(target=lambda:enviar_email_async(prob_id,request.POST.get("solution", "ERROR")))
     thread.start()
     return redirect('Problem', prob_id=prob_id)
